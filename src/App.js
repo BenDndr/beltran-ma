45f17781e64797efb67c9bc5fd8e753ddc7cdc5d
Benjamin DANDRE <dandre.ben@gmail.com>
2021-04-06 17:34:12 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -6,16 +6,70 @@ function App() {
 
   return(
     <div className="app">
-      <div className="Banner">
-        <h1>Marcel Beltran</h1>
-        <h2>Dépanage informatique</h2>
-        <a href="#">Contact</a>
+
+      <div className="banner">
+        <h1>Beltran-ma</h1>
+        <p><strong>Dépanage informatique</strong></p>
+        <a href="#" className="btn">Contact</a>
       </div>
-      <div className="Pres">
+
+      <div className="pres">
+        <h2>Qui sommes nous ?</h2>
         <p>
-          Dépanage pour tout type de pc, sur site ou à l'attelier.
+          Dépanage informatique pour tout type de pc, sur site ou à l'attelier.
         </p>
-        <Card name="billy" message="ché" power="beuré"/>
+        <h3>Notre philosophie</h3>
+        <p>
+          Lorem ipsum
+        </p>
+      </div>
+
+      <div className="prestation">
+        <h2>Nos prestations</h2>
+        <div className="cards">
+          <Card
+            name="Réparation d'appareil"
+            message="Prolongez la durée de vie de votre appareil.
+            Ordinateurs, Smartphones et Tablettes, nous donnons
+            une deuxième vie à vos appareils."
+            />
+          <Card
+            name="Récupération de données"
+            message="Vous avez perdu vos données suite à une mauvaise
+            manipulation ? Pas de soucis on les récupère pour vous."
+            />
+          <Card
+            name="Transfert de films sur DD/DVD"
+            message="Stockez vos données sur des supports physique
+            et conservez un backup de toutes vos données."
+            />
+        </div>
+      </div>
+
+      <div className="expert">
+        <h2>Notre expert</h2>
+        <div className="e-cont">
+          <div className="e-photo"></div>
+          <div className="e-text">
+            <h3>Marcel Beltran</h3>
+            <p>Lorem ipsum</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="contact">
+        <h3>Nous contacter</h3>
+        <p>
+
+        </p>
+      </div>
+
+      <div className="footer">
+        <p>© 2021 beltran-ma.fr</p>
+        <div>
+          <i class="fab fa-facebook-square"></i>
+          <i class="fab fa-twitter-square"></i>
+        </div>
       </div>
 
     </div>
--- src/card.js
+++ src/card.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import "./App.css";
 
-function Card({name, message, power}) {
+function Card({name, message}) {
   return(
-    <div className="Card">
+    <div className="card">
       <h3>{name}</h3>
       <p>{message}</p>
-      <h3>{power}</h3>
     </div>
   );
 }
